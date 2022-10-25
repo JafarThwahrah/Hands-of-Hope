d7df8921b82014f25806f3c43718a55094adce16
DanaAlomari <danaalomari27@gmail.com>
2022-10-25 17:44:33 +0300

    fix product page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    
+
 
 <!-- banner -->
 <div class="heading-container heading-resize heading-button">
@@ -22,22 +22,26 @@
 
 
 
+<div class="text-center mx-auto mb-5" style="max-width: 500px;">
+    <h5 class="d-inline-block text-primary text-uppercase border-bottom border-5">Medical Packages</h5>
+    <h1 class="display-4">Medical Products</h1>
+</div>
 
-<div class="container-fluid py-5">
-    <div class="container d-flex flex-wrap">
+<div class="container ">
+    <div class="container d-flex flex-wrap gap-5 justify-content-center">
         @foreach ($products as $product)
 
-        <div class="bg-light rounded text-center m-3">
+        <div class="bg-light rounded text-center m-1"  style="width: 40%">
             <div class="position-relative">
-                <img class="img-fluid rounded-top" src="data:image/jpg;charset=utf8;base64,{{$product['image']}}" alt="0000000">
+                <img class="img-fluid rounded-top " style="width: 250px" src="data:image/jpg;charset=utf8;base64,{{$product['image']}}" alt="0000000">
             </div>
-            <div class="text-center py-5 m-5">
+            <div class="py-2 m-2">
                 <h3>{{$product['name']}}</h3>
-                <p>{{$product['description']}}</p>
+                <p  class="px-3" style="text-align: justify">{{$product['description']}}</p>
                 <a href="order/{{$product['id']}}" class="btn btn-primary rounded-pill py-3 px-5 my-2">Order Now</a>
             </div>
         </div>
-        
+
 
         @endforeach
 
